6445eafcbde54050015e05b38e41a4763ef50379
sambautomation <sdiordiagne@yahoo.fr>
2023-11-27 10:40:44 +0100

    first commit

--- cypress/e2e/BDD/homepage/hooks.js
+++ cypress/e2e/BDD/homepage/hooks.js
@@ -8,7 +8,7 @@ before(function () {
 beforeEach (function () {
       cy.visit ("/")
       cy.viewport(1740, 1200)
-      cy.get('.osano-cm-accept-all').click()
+    //  cy.get('.osano-cm-accept-all').click()
     }
 )
 
